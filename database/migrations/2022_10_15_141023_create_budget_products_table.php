15295d0a408e2478bed94cb8b14c073600f393d9
Guilherme Andrade <guilhermeandrade.profissional@outlook.com>
2022-10-15 11:12:04 -0300

    feat: create migration budgets and budget products

---
+++ database/migrations/2022_10_15_141023_create_budget_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('budget_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('budgets_id');
+            $table->foreign('budgets_id')->references('id')->on('budgets');
+            $table->unsignedBigInteger('products_id');
+            $table->foreign('products_id')->references('id')->on('products');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('budget_products');
+    }
+};
